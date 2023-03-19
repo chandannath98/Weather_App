6417407736a3b4eb94e06c6a7eb16a0f09afbd6b
chandannath98 <3198nathchandan@gmail.com>
2023-03-19 22:32:02 +0530

    update

--- src/screens/WeatherTimelineScreen.js
+++ src/screens/WeatherTimelineScreen.js
@@ -5,28 +5,70 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import WeatherListItem from "../components/WeatherListItem";
+import { Entypo } from '@expo/vector-icons';
+import { useSelector } from "react-redux";
+import { MenuView } from '@react-native-menu/menu';
 
 
 export default function WeatherTimelineScreen({ navigation, route }) {
 
-  const data=route.params.data
+  const { weatherData } = useSelector(
+    (state) => state.weatherAppReducer
+  );
 
-// console.log(data)
+
+const [data, setData] = useState(route.params.data);
+const [date, setDate] = useState(route.params.date);
+
+
+ const currDate= (new Date(date)).getDate()
+  
+ const prevDate=(new Date((new Date(date)).setDate(currDate-1))).toDateString()
+
+ const nextDate=(new Date((new Date(date)).setDate(currDate+1))).toDateString()
+
 
   return (
     <View style={styles.container}>
+
+
+{weatherData[prevDate] &&
+      <TouchableOpacity 
+      onPress={()=>{
+        setData(weatherData[prevDate])
+        setDate(prevDate)
+        }} 
+      style={styles.leftPaginationIcon} >
+<Entypo name="chevron-thin-left" size={wp("10%")} color="#dee1e6" />
+</TouchableOpacity>
+
+}
+
+{weatherData[nextDate] &&
+<TouchableOpacity
+ style={styles.rightPaginationIcon} 
+onPress={()=>{
+  setData(weatherData[nextDate])
+  setDate(nextDate)
+  }} 
+>
+
+<Entypo name="chevron-thin-right" size={wp("10%")} color="#dee1e6" />
+
+</TouchableOpacity>
+}
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <AntDesign name="leftcircle" size={hp("6%")} color="grey" />
         </TouchableOpacity>
-        <Text style={styles.headerTxt}>Sun, 19 Mar 2023</Text>
+        <Text style={styles.headerTxt}>{date}</Text>
       </View>
 
       <FlatList
@@ -54,5 +96,18 @@ const styles = StyleSheet.create({
     fontSize: hp("3%"),
     marginHorizontal: wp("3%"),
     fontWeight: "700",
+  },
+  leftPaginationIcon:{
+    position:"absolute",
+    left:wp("2%"),
+    top:hp("47%"),
+    zIndex:1
+
+  },
+  rightPaginationIcon:{
+    position:"absolute",
+    right:wp("2%"),
+    top:hp("47%"),
+    zIndex:1
   },
 });
